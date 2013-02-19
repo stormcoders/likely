c03e15887e110645020b2a30a19576551082dd15
Ígor Bonadio <igorbonadio@gmail.com>
2013-02-18 22:36:58 -0300

    probability spec

--- src/test/scala/DiscreteIIDModelSpec.scala
+++
@@ -1,9 +0,0 @@
-import org.scalatest._
-
-class DiscreteIIDModelSpec extends FlatSpec {
-  behavior of "A Discrete IID Model"
-  
-  it should "be initialized by a set of symbols and probabilities" in (pending)
-  it should "evaluate an entire sequence" in (pending)
-  it should "evaluate a sub-sequence" in (pending)
-}
---
+++ src/test/scala/ProbabilitySpec.scala
@@ -0,0 +1,12 @@
+import org.scalatest._
+
+class ProbabilitySpec extends FlatSpec {
+  behavior of "A probability"
+  
+  it should "store its log representation" in (pending)
+  it should "print its normal [0,1] representation" in (pending)
+  it should "multiply probabilities" in (pending)
+  it should "divide probabilities" in (pending)
+  it should "add probabilities" in (pending)
+  it should "subtract probabilities" in (pending)
+}
