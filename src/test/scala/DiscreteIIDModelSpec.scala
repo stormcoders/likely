0a20832bc280404b820a071e6541370777188b72
Ígor Bonadio <igorbonadio@gmail.com>
2013-02-18 22:25:20 -0300

    Discrete IID Model spec

---
+++ src/test/scala/DiscreteIIDModelSpec.scala
@@ -0,0 +1,9 @@
+import org.scalatest._
+
+class DiscreteIIDModelSpec extends FlatSpec {
+  behavior of "A Discrete IID Model"
+  
+  it should "be initialized by a set of symbols and probabilities" in (pending)
+  it should "evaluate an entire sequence" in (pending)
+  it should "evaluate a sub-sequence" in (pending)
+}
