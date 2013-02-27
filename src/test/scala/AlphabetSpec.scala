3a2b7c60c6d6e6d69ad6e2469a2a0639de9a411b
Ígor Bonadio <igorbonadio@gmail.com>
2013-02-27 18:16:56 -0300

    alphabet spec

---
+++ src/test/scala/AlphabetSpec.scala
@@ -0,0 +1,12 @@
+import org.scalatest._
+import org.scalatest.matchers._
+
+class AlphabetSpec extends FlatSpec with ShouldMatchers {
+  behavior of "A alphabet"
+  
+  it should "store a set of symbols" in (pending)
+  it should "return symbol's id " in (pending)
+  it should "return id's symbol" in (pending)
+  it should "generate a sequence of ids" in (pending)
+  it should "generate a sequence of symbols" in (pending)
+}
