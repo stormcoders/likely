37ddf40cf30a06f8d3f8a33b419e758a3336d23a
Ígor Bonadio <igorbonadio@gmail.com>
2013-02-27 18:51:10 -0300

    alphabet returns ids

--- src/main/scala/Alphabet.scala
+++ src/main/scala/Alphabet.scala
@@ -1,3 +1,8 @@
 class Alphabet(symbols: List[String]) {
   val alphabetMap = (symbols zip (0 to (symbols.length-1))).toMap
+  
+  def id(symbol: String):Int = alphabetMap get symbol match {
+    case Some(x) => x
+    case _       => -1
+  }
 }
--- src/test/scala/AlphabetSpec.scala
+++ src/test/scala/AlphabetSpec.scala
@@ -9,7 +9,17 @@ class AlphabetSpec extends FlatSpec with ShouldMatchers {
     casino.alphabetMap.size should be === 2
   }
   
-  it should "return symbol's id " in (pending)
+  it should "return symbol's id " in {
+    val casino = new Alphabet(List("Loaded", "Fair"))
+    casino.id("Loaded") should be === 0
+    casino.id("Fair") should be === 1
+  }
+  
+  it should "return an invalid id if symbol doesn't exist " in {
+    val casino = new Alphabet(List("Loaded", "Fair"))
+    casino.id("Invalid") should be === -1
+  }
+  
   it should "return id's symbol" in (pending)
   it should "generate a sequence of ids" in (pending)
   it should "generate a sequence of symbols" in (pending)
