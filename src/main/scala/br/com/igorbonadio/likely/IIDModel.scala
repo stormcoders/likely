1d61ada7e9e53e5aee58a94d87b17d0768838578
Ígor Bonadio <igorbonadio@gmail.com>
2013-03-14 15:11:04 -0300

    discrete iid model

--- src/main/scala/br/com/igorbonadio/likely/IIDModel.scala
+++ src/main/scala/br/com/igorbonadio/likely/IIDModel.scala
@@ -2,12 +2,16 @@ package br.com.igorbonadio.likely
 
 import util.Random
 
-class IIDModel(distribution: DiscreteDistribution) {
-  def prob(sequence: List[Int]):LogProbability = sequence match {
+abstract class IIDModel[T](distribution: Distribution[T]) {
+  def prob(sequence: List[T]):LogProbability = sequence match {
     case x::xs => distribution.prob(x) * prob(xs)
     case List() => Probability(1)
   }
-  
+  def choose: T
+  def chooseSequence(size: Int): List[T]
+}
+
+class DiscreteIIDModel(distribution: DiscreteDistribution) extends IIDModel(distribution) {
   def choose = {
     def chooseWith(v: Double, prob: List[LogProbability], sym: Int): Int = prob match {
       case p::ps => if ((v - p.expValue) < 0) sym
@@ -17,7 +21,7 @@ class IIDModel(distribution: DiscreteDistribution) {
     chooseWith((new Random()).nextDouble, distribution.probabilities, 0)
   }
   
-  def chooseSequence(size: Int): List[Int] = 
+  def chooseSequence(size: Int) = 
     if (size > 0) choose :: chooseSequence(size-1)
     else List()
 }
--- src/test/scala/br/com/igorbonadio/likely/IIDModelSpec.scala
+++ src/test/scala/br/com/igorbonadio/likely/IIDModelSpec.scala
@@ -3,13 +3,13 @@ package br.com.igorbonadio.likely
 import org.scalatest._
 import org.scalatest.matchers._
 
-class IIDModelSpec extends FlatSpec with ShouldMatchers {
-  behavior of "An IID Model"
+class DiscreteIIDModelSpec extends FlatSpec with ShouldMatchers {
+  behavior of "A Discrete IID Model"
   
   it should "get the probability of a given sequence" in {
     val alphabet = new Alphabet(List("Loaded", "Fair"))
     val distribution = new DiscreteDistribution(List(Probability(0.8), Probability(0.2)))
-    val model = new IIDModel(distribution)
+    val model = new DiscreteIIDModel(distribution)
     val sequence = alphabet.generateSequeceOfIds(List("Fair", "Loaded"))
     
     model.prob(sequence).expValue should be (0.16 plusOrMinus 0.0001)
@@ -18,7 +18,7 @@ class IIDModelSpec extends FlatSpec with ShouldMatchers {
   it should "generate a random symbol" in {
     val alphabet = new Alphabet(List("Loaded", "Fair"))
     val distribution = new DiscreteDistribution(List(Probability(0.8), Probability(0.2)))
-    val model = new IIDModel(distribution)
+    val model = new DiscreteIIDModel(distribution)
     
     model.choose should (be === 0 or be === 1)
   }
@@ -26,8 +26,9 @@ class IIDModelSpec extends FlatSpec with ShouldMatchers {
   it should "generate a random sequence" in {
     val alphabet = new Alphabet(List("Loaded", "Fair"))
     val distribution = new DiscreteDistribution(List(Probability(0.8), Probability(0.2)))
-    val model = new IIDModel(distribution)
+    val model = new DiscreteIIDModel(distribution)
     
     model.chooseSequence(5).foreach(sym => sym should (be === 0 or be === 1))
   }
 }
+
