7257d6dd734d9fde66102a160465c898e68676e2
Ígor Bonadio <igorbonadio@gmail.com>
2013-03-14 14:54:19 -0300

    fix spelling

--- src/main/scala/br/com/igorbonadio/likely/DiscreteDistribution.scala
+++ src/main/scala/br/com/igorbonadio/likely/DiscreteDistribution.scala
@@ -2,7 +2,7 @@ package br.com.igorbonadio.likely
 
 class DiscreteDistribution(probs: List[LogProbability]) extends Distribution[Int] {
   val probabilities = probs
-  val probbabilityMap = ((0 to (probabilities.length - 1)) zip probabilities).toMap
+  val probabilityMap = ((0 to (probabilities.length - 1)) zip probabilities).toMap
   
-  def prob(id: Int):LogProbability = probbabilityMap(id)
+  def prob(id: Int):LogProbability = probabilityMap(id)
 }
