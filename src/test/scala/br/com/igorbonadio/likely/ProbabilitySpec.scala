c0981c9a2eb1308e502f8904ca3ff16820f802b6
Ígor Bonadio <igorbonadio@gmail.com>
2013-03-30 14:29:46 -0300

    refactoring ProbabilitySpec's normal representation

--- src/test/scala/br/com/igorbonadio/likely/ProbabilitySpec.scala
+++ src/test/scala/br/com/igorbonadio/likely/ProbabilitySpec.scala
@@ -13,16 +13,9 @@ class ProbabilitySpec extends FlatSpec with ShouldMatchers {
   }
   
   it should "show its normal [0,1] representation" in {
-    Probability(1.0).expValue should be (1.0 plusOrMinus 0.0001)
-    Probability(0.9).expValue should be (0.9 plusOrMinus 0.0001)
-    Probability(0.8).expValue should be (0.8 plusOrMinus 0.0001)
-    Probability(0.7).expValue should be (0.7 plusOrMinus 0.0001)
-    Probability(0.6).expValue should be (0.6 plusOrMinus 0.0001)
-    Probability(0.5).expValue should be (0.5 plusOrMinus 0.0001)
-    Probability(0.4).expValue should be (0.4 plusOrMinus 0.0001)
-    Probability(0.3).expValue should be (0.3 plusOrMinus 0.0001)
-    Probability(0.2).expValue should be (0.2 plusOrMinus 0.0001)
-    Probability(0.1).expValue should be (0.1 plusOrMinus 0.0001)
+    (1 to 10).map {i => i/10.0}.foreach {
+      p => Probability(p).expValue should be (p plusOrMinus 0.0001)
+    }
   }
   
   it should "multiply probabilities" in {
