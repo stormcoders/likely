00a3f231fb9b8d9f154c52e2e49212c032147c60
Ígor Bonadio <igorbonadio@gmail.com>
2013-03-30 12:05:37 -0300

    refactoring NormalDistributionSpec

--- src/test/scala/br/com/igorbonadio/likely/NormalDistributionSpec.scala
+++ src/test/scala/br/com/igorbonadio/likely/NormalDistributionSpec.scala
@@ -6,16 +6,17 @@ import org.scalatest.matchers._
 class NormalDistributionSpec extends FlatSpec with ShouldMatchers {
   behavior of "A normal distribution"
   
-  it should "get the probability of a given symbol" in {
+  trait DistributionFixture {
     val distribution = new NormalDistribution(0, 1)
+  }
+
+  it should "get the probability of a given symbol" in new DistributionFixture {
     distribution.prob(2).expValue should be (0.0539 plusOrMinus 0.0001)
     distribution.prob(1).expValue should be (0.2419 plusOrMinus 0.0001)
     distribution.prob(-0.5).expValue should be (0.3520 plusOrMinus 0.0001)
   }
 
-  it should "choose a symbol" in {
-    val distribution = new NormalDistribution(0, 1)
-    
+  it should "choose a symbol" in new DistributionFixture {
     distribution.choose should not be === (distribution.choose)
   }
 }
