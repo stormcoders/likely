506340abde3f77c5193a0463eaa652f0818d71f1
Ígor Bonadio <igorbonadio@gmail.com>
2013-03-30 11:59:55 -0300

    Refactoring AlphabetSpec

--- src/test/scala/br/com/igorbonadio/likely/AlphabetSpec.scala
+++ src/test/scala/br/com/igorbonadio/likely/AlphabetSpec.scala
@@ -5,51 +5,46 @@ import org.scalatest.matchers._
 
 class AlphabetSpec extends FlatSpec with ShouldMatchers {
   behavior of "A alphabet"
-  
-  it should "store a set of symbols" in {
+
+  trait Casino {
     val casino = new Alphabet(List("Loaded", "Fair"))
+  }
+  
+  it should "store a set of symbols" in new Casino {
     casino.alphabetMap.size should be === 2
   }
 
-  it should "have a string representation" in {
-    val casino = new Alphabet(List("Loaded", "Fair"))
+  it should "have a string representation" in new Casino {
     casino.toString should be === "Alphabet(Loaded, Fair)"
   }
   
-  it should "return the number of symbols" in {
-    val casino = new Alphabet(List("Loaded", "Fair"))
+  it should "return the number of symbols" in new Casino {
     casino.size should be === 2
   }
   
-  it should "return symbol's id " in {
-    val casino = new Alphabet(List("Loaded", "Fair"))
+  it should "return symbol's id " in new Casino {
     casino.id("Loaded") should be === 0
     casino.id("Fair") should be === 1
   }
   
-  it should "return an invalid id if symbol doesn't exist " in {
-    val casino = new Alphabet(List("Loaded", "Fair"))
+  it should "return an invalid id if symbol doesn't exist " in new Casino {
     casino.id("Invalid") should be === -1
   }
   
-  it should "return id's symbol" in {
-    val casino = new Alphabet(List("Loaded", "Fair"))
+  it should "return id's symbol" in new Casino {
     casino.symbol(0) should be === "Loaded"
     casino.symbol(1) should be === "Fair"
   }
   
-  it should "return an empty string if id is invalid" in {
-    val casino = new Alphabet(List("Loaded", "Fair"))
+  it should "return an empty string if id is invalid" in new Casino {
     casino.symbol(2) should be === ""
   }
 
-  it should "generate a stream of symbols" in {
-    val casino = new Alphabet(List("Loaded", "Fair"))
+  it should "generate a stream of symbols" in new Casino {
     casino.generateSequeceOfSymbols(Stream(1,1, 0, 0, 1)).toList should be === List("Fair", "Fair", "Loaded", "Loaded", "Fair")
   }
 
-  it should "generate a stream of ids" in {
-    val casino = new Alphabet(List("Loaded", "Fair"))
+  it should "generate a stream of ids" in new Casino {
     casino.generateSequeceOfIds(Stream("Fair", "Fair", "Loaded", "Loaded", "Fair")).toList should be === List(1,1, 0, 0, 1)
   }
 }
