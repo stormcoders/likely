1c7fa0d58febb31a264c60410efcc3578e4fc23d
Ígor Bonadio <igorbonadio@gmail.com>
2013-03-30 11:38:13 -0300

    IIDModel uses distribution to generate a random symbol/value

--- src/main/scala/br/com/igorbonadio/likely/Distribution.scala
+++ src/main/scala/br/com/igorbonadio/likely/Distribution.scala
@@ -2,4 +2,5 @@ package br.com.igorbonadio.likely
 
 trait Distribution[T] {
   def prob(x: T): LogProbability
+  def choose: T
 }
--- src/main/scala/br/com/igorbonadio/likely/IIDModel.scala
+++ src/main/scala/br/com/igorbonadio/likely/IIDModel.scala
@@ -7,32 +7,10 @@ abstract class IIDModel[T](distribution: Distribution[T]) {
     case x #:: xs => distribution.prob(x) * prob(xs)
     case Stream() => Probability(1)
   }
-  
-  def choose: T
 
-  def chooseStream: Stream[T] = choose #:: chooseStream
+  def choose: T = distribution.choose
+  def chooseStream: Stream[T] = distribution.choose #:: chooseStream
 }
 
-class DiscreteIIDModel(distribution: DiscreteDistribution) extends IIDModel(distribution) {
-  def choose = {
-    def chooseWith(v: Double, prob: List[LogProbability], sym: Int): Int = prob match {
-      case p::ps => if ((v - p.expValue) < 0) sym
-              else chooseWith(v - p.expValue, ps, sym + 1)
-      case List() => sym
-    }
-    chooseWith((new Random()).nextDouble, distribution.probabilities, 0)
-  }
-}
-
-class ContinuousIIDModel(distribution: NormalDistribution) extends IIDModel(distribution) {
-  def choose = {
-    def boxMuller = {
-      val uniform = new Random()
-      val u1 = uniform.nextDouble
-      val u2 = uniform.nextDouble
-      val r = math.sqrt(-2*math.log(u1))
-      math.sqrt(-2*math.log(u1))*math.cos(2*math.Pi*u2)
-    }
-    distribution.createMember(boxMuller)
-  }
-}
+class DiscreteIIDModel(distribution: DiscreteDistribution) extends IIDModel(distribution)
+class ContinuousIIDModel(distribution: NormalDistribution) extends IIDModel(distribution)
