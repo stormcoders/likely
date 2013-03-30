9286651d10165e9e7363c8a349ee30e986dc5de4
Ígor Bonadio <igorbonadio@gmail.com>
2013-03-30 11:34:21 -0300

    Normal distribution generates a random value

--- src/main/scala/br/com/igorbonadio/likely/NormalDistribution.scala
+++ src/main/scala/br/com/igorbonadio/likely/NormalDistribution.scala
@@ -1,9 +1,22 @@
 package br.com.igorbonadio.likely
 
+import util.Random
+
 class NormalDistribution(mean: Double, sd: Double) extends Distribution[Double] {
   def createMember(x: Double) =
     mean + x*sd
     
   def prob(x: Double) = 
     new LogProbability(((x-mean)*(x-mean)/(2*sd*sd)) + math.log(sd*math.sqrt(2*math.Pi)) )
+
+  def choose : Double = {
+    def boxMuller = {
+      val uniform = new Random()
+      val u1 = uniform.nextDouble
+      val u2 = uniform.nextDouble
+      val r = math.sqrt(-2*math.log(u1))
+      math.sqrt(-2*math.log(u1))*math.cos(2*math.Pi*u2)
+    }
+    createMember(boxMuller)
+  }
 }
--- src/test/scala/br/com/igorbonadio/likely/NormalDistributionSpec.scala
+++ src/test/scala/br/com/igorbonadio/likely/NormalDistributionSpec.scala
@@ -12,4 +12,10 @@ class NormalDistributionSpec extends FlatSpec with ShouldMatchers {
     distribution.prob(1).expValue should be (0.2419 plusOrMinus 0.0001)
     distribution.prob(-0.5).expValue should be (0.3520 plusOrMinus 0.0001)
   }
+
+  it should "choose a symbol" in {
+    val distribution = new NormalDistribution(0, 1)
+    
+    distribution.choose should not be === (distribution.choose)
+  }
 }
