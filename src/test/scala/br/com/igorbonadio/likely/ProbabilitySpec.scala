8fa331931d17d12ad34f072bbd76b6205c054ed5
Ígor Bonadio <igorbonadio@gmail.com>
2013-03-31 10:41:11 -0300

    using 'for' to improve multipication of probability tests

--- src/test/scala/br/com/igorbonadio/likely/ProbabilitySpec.scala
+++ src/test/scala/br/com/igorbonadio/likely/ProbabilitySpec.scala
@@ -7,23 +7,21 @@ class ProbabilitySpec extends FlatSpec with ShouldMatchers {
   behavior of "A probability"
   
   it should "store its log representation" in {
-    (1 to 10).map {i => i/10.0}.foreach {
-      p => Probability(p).logValue should be (-math.log(p) plusOrMinus 0.0001)
+    (1 to 10).map {i => i/10.0}.foreach { p => 
+      Probability(p).logValue should be (-math.log(p) plusOrMinus 0.0001)
     }
   }
   
   it should "show its normal [0,1] representation" in {
-    (1 to 10).map {i => i/10.0}.foreach {
-      p => Probability(p).expValue should be (p plusOrMinus 0.0001)
+    (1 to 10).map {i => i/10.0}.foreach { p => 
+      Probability(p).expValue should be (p plusOrMinus 0.0001)
     }
   }
   
   it should "multiply probabilities" in {
-    (Probability(0.5) * Probability(0.2)).logValue should be (Probability(0.10).logValue plusOrMinus 0.0001)
-    (Probability(0.2) * Probability(0.2)).logValue should be (Probability(0.04).logValue plusOrMinus 0.0001)
-    (Probability(0.5) * Probability(0.5)).logValue should be (Probability(0.25).logValue plusOrMinus 0.0001)
-    (Probability(1.0) * Probability(0.2)).logValue should be (Probability(0.20).logValue plusOrMinus 0.0001)
-    (Probability(0.5) * Probability(1.0)).logValue should be (Probability(0.50).logValue plusOrMinus 0.0001)
+    (for (x <- 1 to 10; y <- 1 to 10) yield Pair(x/10.0, y/10.0)).foreach { p => p match {
+      case Pair(x, y) => (Probability(x) * Probability(y)).logValue should be (Probability(x*y).logValue plusOrMinus 0.0001)
+    }}
   }
   
   it should "divide probabilities" in {
