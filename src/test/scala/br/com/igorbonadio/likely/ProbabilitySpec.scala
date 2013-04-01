acf5f832bd1484fbf3590eec77532fe548fe0d25
Ígor Bonadio <igorbonadio@gmail.com>
2013-04-01 11:40:36 -0300

    better probability's string representation test

--- src/test/scala/br/com/igorbonadio/likely/ProbabilitySpec.scala
+++ src/test/scala/br/com/igorbonadio/likely/ProbabilitySpec.scala
@@ -43,7 +43,7 @@ class ProbabilitySpec extends FlatSpec with ShouldMatchers {
   }
   
   it should "have a string representation" in {
-    Probability(0.5).toString should be === "0.5"
-    Probability(0.2).toString should be === "0.2"
+    Probability(0.5).toString.toDouble should be (0.5 plusOrMinus 0.0001)
+    Probability(0.2).toString.toDouble should be (0.2 plusOrMinus 0.0001)
   }
 }
