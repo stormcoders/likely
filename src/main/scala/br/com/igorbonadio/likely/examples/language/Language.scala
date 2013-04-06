3bd437a0a7d407192bb51b5190fd681aec63b903
Ígor Bonadio <igorbonadio@gmail.com>
2013-04-05 21:21:13 -0300

    improving portuguese-english example

--- src/main/scala/br/com/igorbonadio/likely/examples/language/Language.scala
+++ src/main/scala/br/com/igorbonadio/likely/examples/language/Language.scala
@@ -2,24 +2,19 @@ package br.com.igorbonadio.likely.examples
 
 import br.com.igorbonadio.likely._
 
-object Language {
+class LanguageClassifier {
+  // training set
+  val pt = scala.io.Source.fromFile("src/main/scala/br/com/igorbonadio/likely/examples/language/pt.txt").
+    mkString.toUpperCase.toList.filter(c => c.isLetter && c <= 'z').map(x => x.toString)
+  val en = scala.io.Source.fromFile("src/main/scala/br/com/igorbonadio/likely/examples/language/en.txt").
+    mkString.toUpperCase.toList.filter(c => c.isLetter && c <= 'z').map(x => x.toString)
+  val alphabet = new Alphabet(List("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
+                                   "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"))
+  val ptModel = DiscreteIIDModel.train(alphabet.generateSequeceOfIds(pt.toStream))
+  val enModel = DiscreteIIDModel.train(alphabet.generateSequeceOfIds(en.toStream))
 
   def classify(text: String, debug: Boolean) = {
-    // training set
-    val pt = scala.io.Source.fromFile("src/main/scala/br/com/igorbonadio/likely/examples/language/pt.txt").
-      mkString.toUpperCase.toList.filter(c => c.isLetter && c <= 'z').map(x => x.toString)
-    val en = scala.io.Source.fromFile("src/main/scala/br/com/igorbonadio/likely/examples/language/en.txt").
-      mkString.toUpperCase.toList.filter(c => c.isLetter && c <= 'z').map(x => x.toString)
-
-    val alphabet = new Alphabet(List("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
-                                     "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"))
-
-    val ptModel = DiscreteIIDModel.train(alphabet.generateSequeceOfIds(pt.toStream))
-    val enModel = DiscreteIIDModel.train(alphabet.generateSequeceOfIds(en.toStream))
-
-    val test = text.
-      toUpperCase.toList.filter(c => c.isLetter && c <= 'z').map(x => x.toString)
-
+    val test = text.toUpperCase.toList.filter(c => c.isLetter && c <= 'z').map(x => x.toString)
     val ptProb = ptModel.prob(alphabet.generateSequeceOfIds(test.toStream))
     val enProb = enModel.prob(alphabet.generateSequeceOfIds(test.toStream))
     
@@ -30,13 +25,14 @@ object Language {
       println(enProb)
     }
 
-    if (ptProb > enProb)
-      "pt"
-    else
-      "en"
+    if (ptProb > enProb) "pt" 
+    else "en"
   }
+}
 
+object Language {
   def main(args: Array[String]) {
-    println("classification = " + classify(args(0), true))
+    val classifier = new LanguageClassifier
+    println("classification = " + classifier.classify(args(0), true))
   }
 }
--- src/test/scala/br/com/igorbonadio/likely/examples/language/LanguageSpec.scala
+++ src/test/scala/br/com/igorbonadio/likely/examples/language/LanguageSpec.scala
@@ -6,18 +6,23 @@ import org.scalatest.matchers._
 class LanguageSpec extends FlatSpec with ShouldMatchers {
   behavior of "A Language Classifier"
 
-  it should "classify portuguese texts" in {
-    Language.classify("Pequena, eu te amo!", false) should be === "pt"
-    Language.classify("Hoje comi peixei.", false) should be === "pt"
-    Language.classify("Nao posso perder a novela hoje!!!", false) should be === "pt"
-    Language.classify("Nao existe lugar como nossa casa...", false) should be === "pt"
-    Language.classify("Isso funciona! Incrivel!!!", false) should be === "pt"
+  trait PortugueseEnglishClassifier {
+    val classifier = new LanguageClassifier
   }
-  it should "classify english texts" in {
-    Language.classify("Baby, I love you!", false) should be === "en"
-    Language.classify("Today, I ate fish.", false) should be === "en"
-    Language.classify("I cant miss the today's episode of the soap opera!!!", false) should be === "en"
-    Language.classify("There is no place like home", false) should be === "en"
-    Language.classify("It works! Awesome!!!", false) should be === "en"
+
+  it should "classify portuguese texts" in new PortugueseEnglishClassifier {
+    classifier.classify("Pequena, eu te amo!", false) should be === "pt"
+    classifier.classify("Hoje comi peixei.", false) should be === "pt"
+    classifier.classify("Nao posso perder a novela hoje!!!", false) should be === "pt"
+    classifier.classify("Nao existe lugar como nossa casa...", false) should be === "pt"
+    classifier.classify("Isso funciona! Incrivel!!!", false) should be === "pt"
+  }
+
+  it should "classify english texts" in new PortugueseEnglishClassifier {
+    classifier.classify("Baby, I love you!", false) should be === "en"
+    classifier.classify("Today, I ate fish.", false) should be === "en"
+    classifier.classify("I cant miss the today's episode of the soap opera!!!", false) should be === "en"
+    classifier.classify("There is no place like home", false) should be === "en"
+    classifier.classify("It works! Awesome!!!", false) should be === "en"
   }
 }
