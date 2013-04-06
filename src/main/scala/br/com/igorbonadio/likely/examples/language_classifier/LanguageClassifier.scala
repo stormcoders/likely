b4a301f3de0c8226bb6c50f8177f1e12d8554baa
Ígor Bonadio <igorbonadio@gmail.com>
2013-04-06 09:57:14 -0300

    refactoring: justLetter is a method that remove all the characteres that are not letters

--- src/main/scala/br/com/igorbonadio/likely/examples/language_classifier/LanguageClassifier.scala
+++ src/main/scala/br/com/igorbonadio/likely/examples/language_classifier/LanguageClassifier.scala
@@ -4,17 +4,18 @@ import br.com.igorbonadio.likely._
 
 class LanguageClassifier {
   // training set
-  val pt = scala.io.Source.fromFile("src/main/scala/br/com/igorbonadio/likely/examples/language_classifier/pt.txt").
-    mkString.toUpperCase.toList.filter(c => c.isLetter && c <= 'z').map(x => x.toString)
-  val en = scala.io.Source.fromFile("src/main/scala/br/com/igorbonadio/likely/examples/language_classifier/en.txt").
-    mkString.toUpperCase.toList.filter(c => c.isLetter && c <= 'z').map(x => x.toString)
+  val pt = justLetters(scala.io.Source.fromFile("src/main/scala/br/com/igorbonadio/likely/examples/language_classifier/pt.txt").mkString)
+  val en = justLetters(scala.io.Source.fromFile("src/main/scala/br/com/igorbonadio/likely/examples/language_classifier/en.txt").mkString)
   val alphabet = new Alphabet(List("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
                                    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"))
   val ptModel = DiscreteIIDModel.train(alphabet.generateSequeceOfIds(pt.toStream))
   val enModel = DiscreteIIDModel.train(alphabet.generateSequeceOfIds(en.toStream))
 
+  def justLetters(sequence: String) =
+    sequence.toUpperCase.toList.filter(c => c.isLetter && c <= 'z').map(x => x.toString)
+
   def classify(text: String, debug: Boolean) = {
-    val test = text.toUpperCase.toList.filter(c => c.isLetter && c <= 'z').map(x => x.toString)
+    val test = justLetters(text)
     val ptProb = ptModel.prob(alphabet.generateSequeceOfIds(test.toStream))
     val enProb = enModel.prob(alphabet.generateSequeceOfIds(test.toStream))
     
