adea0c43aac51f46dc5467225ac87671e1d7e2cb
Ígor Bonadio <igorbonadio@gmail.com>
2013-05-06 18:03:59 -0300

    less verbose hmm definition

--- src/test/scala/br/com/igorbonadio/likely/HiddenMarkovModelSpec.scala
+++ src/test/scala/br/com/igorbonadio/likely/HiddenMarkovModelSpec.scala
@@ -11,29 +11,29 @@ class HiddenMarkovModelSpec extends FlatSpec with ShouldMatchers {
   it should "evaluate the joint probability of a sequence of labels and a sequence of simbols" in {
     val states = Alphabet("Loaded", "Fair")
     val symbols = Alphabet(1 to 6)
-    val emissions = ConditionalProbabilities(symbols, states) { Prob =>
-      Prob("1", "Loaded") is 50.0.%%
-      Prob("2", "Loaded") is 10.0.%%
-      Prob("3", "Loaded") is 10.0.%%
-      Prob("4", "Loaded") is 10.0.%%
-      Prob("5", "Loaded") is 10.0.%%
-      Prob("6", "Loaded") is 10.0.%%
-      Prob("1", "Fair") is 16.66.%%
-      Prob("2", "Fair") is 16.66.%%
-      Prob("3", "Fair") is 16.66.%%
-      Prob("4", "Fair") is 16.66.%%
-      Prob("5", "Fair") is 16.66.%%
-      Prob("6", "Fair") is 16.66.%%
-    }
-    val transitions = ConditionalProbabilities(states, states) { Prob =>
-      Prob("Loaded", "Loaded") is 5.0.%%
-      Prob("Fair"  , "Loaded") is 95.0.%%
-      Prob("Loaded", "Fair") is 10.0.%%
-      Prob("Fair"  , "Fair") is 90.0.%%
-    }
+    val hmm = new HiddenMarkovModel(
+      ConditionalProbabilities(symbols, states) { Prob =>
+        Prob("1", "Loaded") is 50.0.%%
+        Prob("2", "Loaded") is 10.0.%%
+        Prob("3", "Loaded") is 10.0.%%
+        Prob("4", "Loaded") is 10.0.%%
+        Prob("5", "Loaded") is 10.0.%%
+        Prob("6", "Loaded") is 10.0.%%
+        Prob("1", "Fair") is 16.66.%%
+        Prob("2", "Fair") is 16.66.%%
+        Prob("3", "Fair") is 16.66.%%
+        Prob("4", "Fair") is 16.66.%%
+        Prob("5", "Fair") is 16.66.%%
+        Prob("6", "Fair") is 16.66.%%
+      }, 
+      ConditionalProbabilities(states, states) { Prob =>
+        Prob("Loaded", "Loaded") is 5.0.%%
+        Prob("Fair"  , "Loaded") is 95.0.%%
+        Prob("Loaded", "Fair") is 10.0.%%
+        Prob("Fair"  , "Fair") is 90.0.%%
+      })
     val x = symbols.generateSequeceOfIds(Stream("1", "2"))
     val y = states.generateSequeceOfIds(Stream("Fair", "Loaded"))
-    val hmm = new HiddenMarkovModel(emissions, transitions)
     hmm.prob(x, y).expValue should be ((0.95*(1.0/6)*0.1*0.1) plusOrMinus 0.0001)
   }
 }
